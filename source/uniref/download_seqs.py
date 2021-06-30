9939b030d48b78c105180c4338337a5ca788ccfb
Pablo Gainza <pgainza@Pablos-MacBook-Pro.local>
2021-06-30 15:34:41 +0200

    updated download and verify sequence

--- source/uniref/download_seqs.py
+++ source/uniref/download_seqs.py
@@ -1,32 +1,29 @@
 import os 
 import wget 
+import pandas as pd
 gene_to_uniref_acc = {}
 uniref_acc_to_gene = {}
 all_acc = set()
-with open ('mappings.csv') as f: 
-    for line in f.readlines(): 
-        fields = line.split(',')
-        genename = fields[2].rstrip()
-        acc = fields[0]
-        gene_to_uniref_acc[genename] = acc
-        uniref_acc_to_gene[acc] = genename
-        all_acc.add(acc)
-
-for gene in gene_to_uniref_acc.keys():
-    uniref_fn = f'{gene}.uniref'
-    with open(os.path.join(gene, uniref_fn)) as f2: 
-        print(f'{gene}')
-        for line2 in f2.readlines():
-            acc = line2.rstrip()
-            if gene == 'DRD1':
-                result = wget.download(f"https://www.uniprot.org/uniprot/?query=accession:{acc}&format=fasta", \
-                            os.path.join(gene, f'cluster_{acc}.fasta'))
-            else:
-                continue
-#            elif not os.path.exists(os.path.join(gene, f'cluster_{acc}.fasta')):
-#                result = wget.download(f"https://www.uniprot.org/uniprot/?query=cluster:(uniprot:{acc}%20identity:0.5)&format=fasta", \
-#                            os.path.join(gene, f'cluster_{acc}.fasta'))
-#                print(result)
-#                break
-        print(' ')
+df = pd.read_csv('../ground_truth.csv')
+for ix, row in df.iterrows():
+    name = row['HGNC']
+    print(name)
+    acc = row['UniprotAcc']
+    uname = row['UniprotName']
+    gene_to_uniref_acc[name] = acc
+    uniref_acc_to_gene[acc] = name
+    all_acc.add(acc)
+    if not os.path.exists(name):
+        os.makedirs(name)
+    # Download sequence of human protein. 
+    out_human = os.path.join(os.path.join(name, f'{acc}.fasta'))
+    if not os.path.exists(out_human):
+        result = wget.download(f"https://www.uniprot.org/uniprot/?query=accession:{acc}&format=fasta", \
+               os.path.join(name, f'{acc}.fasta'))
+    # Download cluster. 
+    out_cluster = os.path.join(name, f'cluster_{acc}.fasta')
+    if not os.path.exists(out_cluster):
+        result = wget.download(f"https://www.uniprot.org/uniprot/?query=cluster:(uniprot:{acc}%20identity:0.5)&format=fasta", \
+            out_cluster)
+        print(f'Result = {result}')
 
--- source/uniref/verify_data.py
+++ source/uniref/verify_data.py
@@ -1,22 +1,48 @@
 import os 
+import pandas as pd
+from Bio import SeqIO
 gene_to_uniref_acc = {}
 uniref_acc_to_gene = {}
-all_acc = set()
-with open ('mappings.csv') as f: 
-    for line in f.readlines(): 
-        fields = line.split(',')
-        genename = fields[2].rstrip()
-        acc = fields[0]
-        gene_to_uniref_acc[genename] = acc
-        uniref_acc_to_gene[acc] = genename
-        all_acc.add(acc)
+all_human_acc = set()
+all_acc_by_cluster = {}
+df = pd.read_csv('../ground_truth.csv')
+acc_to_gene = {}
 
-for gene in gene_to_uniref_acc.keys():
-    uniref_fn = f'{gene}.uniref'
-    with open(os.path.join(gene, uniref_fn)) as f2: 
-        print(f'{gene}')
-        for line2 in f2.readlines():
-            if line2.rstrip() in all_acc: 
-                print(f'\t{uniref_acc_to_gene[line2.rstrip()]}')
-        print(' ')
+for ix, row in df.iterrows():
+    name = row['HGNC']
+    print(name)
+    acc = row['UniprotAcc']
+    uname = row['UniprotName']
+    gene_to_uniref_acc[name] = acc
+    uniref_acc_to_gene[acc] = name
+    all_human_acc.add(acc)
+    in_human = os.path.join(os.path.join(name, f'{acc}.fasta'))
+    in_cluster = os.path.join(name, f'cluster_{acc}.fasta')
+    acc_to_gene[acc] = name
+    if not (os.path.exists(in_human)): 
+        print(f"Fasta doesnt exist for {name} {acc}")
+        continue
+    if not (os.path.exists(in_cluster)): 
+        print(f"Cluster doesnt exist for {name} {acc}")
+        continue
+    # Open cluster_fasta
+    num_records_human = 0
+    for record in SeqIO.parse(in_human, "fasta"):
+        num_records_human += 1
+    num_records_cluster = 0
+    all_acc_by_cluster[acc] = []
+    for record in SeqIO.parse(in_cluster, "fasta"):
+        all_acc_by_cluster[acc].append(record.id.split('|')[1])
+        num_records_cluster+= 1
+    print(f"{name} {acc} num_records_human_seq: {num_records_human}, num_records_cluster: {num_records_cluster}")
 
+errors = ''
+for key in all_acc_by_cluster:
+    if key not in all_acc_by_cluster[key]:
+        errors += f'error, {acc_to_gene[key]} ({key}) is not in its own cluster!\n'
+    for acc in all_human_acc: 
+        if acc in all_acc_by_cluster[key]:
+            print(f'{acc} in the cluster of {acc}')
+    print(' ')
+    
+print(errors)
